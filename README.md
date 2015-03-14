aa03b7d2dd079e6c6bc4364e166723e66ca8b888
unknown <405763281@qq.com>
2015-03-14 21:26:20 +0800

    面向对象1.

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # 文档
 
-## 子函数功能
-
+## class Calculate //自定义计算器类
 ````
-void printff(stack<int> c);//打印栈中所有元素
-string change(string infix, string suffix);//实行将中缀表达式转变成后缀表达式
+private:
+string infix;//输入的将要被求值的中缀表达式
 bool checkNum(char ch);//判断是否为数字
 bool checkOperator(char ch);//判断是否为操作符
-bool checkLBracket(char ch);//判断是否为左括号
-bool checkRBracket(char ch);//判断是否为右括号
+bool checkBracket(char ch);//判断是否为括号
 bool checkStack(char ch, stack<char> tempStack);//判断是否将当前的操作符入栈
 bool checkPriority(char one, char two);//比较这两个操作符的优先级
 int getPriority(char temp);//获取当前操作符的优先级别
-int getValue(string suffix);//计算当前后缀表达式的值
 int charToInt(char ch);//将字符类型的数字转换成整型
 int calculate(char opera, int a, int b);//计算数值a和数值b的(operate)操作
-bool checkError(string infix);//检查输入的中缀表达式是否有错
-bool stateNum(string infix, int i, int& lb);//当前字符为数字的状态
-bool stateOper(string infix, int i, int& lb);//当前字符为操作符的状态
-bool stateLBra(string infix, int i, int& lb);//当前字符为左括号的状态
-bool stateRBra(string infix, int i, int& lb);//当前字符为右括号的状态
+bool stateNum(int i, int& lb);//当前字符为数字的状态
+bool stateOper(int i, int& lb);//当前字符为操作符的状态
+bool stateLBra(int i, int& lb);//当前字符为左括号的状态
+bool stateRBra(int i, int& lb);//当前字符为右括号的状态
+
+public:
+void inspirit(string s);//改变中缀表达式
+void pri();//打印中缀表达式
+string change();//实行将中缀表达式转变成后缀表达式
+int getValue();//计算当前后缀表达式的值
+bool checkError();//检查输入的中缀表达式是否有错
+
 ````
--- main.cpp
+++ main.cpp
@@ -4,317 +4,297 @@
 
 using namespace std;
 
-string change(string infix, string suffix);
+class Calculate {
+private:
+    string infix;
 
-bool checkNum(char ch);
-
-bool checkOperator(char ch);
-
-bool checkLBracket(char ch);
-
-bool checkRBracket(char ch);
-
-bool checkStack(char ch, stack<char> tempStack);
-
-bool checkPriority(char one, char two);
-
-int getPriority(char temp);
-
-int getValue(string suffix);
-
-int charToInt(char ch);
-
-int calculate(char opera, int a, int b);
-
-bool checkError(string infix);
-
-bool stateNum(string infix, int i, int &lb);
-
-bool stateOper(string infix, int i, int &lb);
-
-bool stateLBra(string infix, int i, int &lb);
-
-bool stateRBra(string infix, int i, int &lb);
-
-int main() {
-    string expression;
-
-    cin >> expression;
-    expression += '#';
-
-    while (!checkError(expression)) {
-        cout << "error" << endl << "input again" << endl;
-        cin >> expression;
-        expression += '#';
+    bool checkNum(char ch) {
+        if ('0' <= ch && ch <= '9')
+            return true;
+        else
+            return false;
     }
-    string suffixExpression = expression;
 
-    suffixExpression = change(expression, suffixExpression);
-
-    cout << suffixExpression << endl;
-
-    int result = getValue(suffixExpression);
-    cout << result;
-
-    return 0;
-}
-
-bool checkNum(char ch) {
-    if ('0' <= ch && ch <= '9')
-        return true;
-    else
-        return false;
-}
-
-bool checkOperator(char ch) {
-    if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
-        return true;
-    else
-        return false;
-}
-
-bool checkLBracket(char ch) {
-    if (ch == '(')
-        return true;
-    else
-        return false;
-}
-
-bool checkRBracket(char ch) {
-    if (ch == ')')
-        return true;
-    else
-        return false;
-}
-
-bool checkStack(char ch, stack<char> tempStack) {
-    if (tempStack.empty() == true || checkPriority(ch, tempStack.top()) == true)
-        return true;
-    else
-        return false;
-}
-
-bool checkPriority(char one, char two) {
-    if (getPriority(one) > getPriority(two))
-        return true;
-    else
-        return false;
-}
-
-int getPriority(char temp) {
-    int priority = 0;
-    switch (temp) {
-        case '+' :
-            priority = 1;
-            break;
-        case '-' :
-            priority = 1;
-            break;
-        case '*' :
-            priority = 2;
-            break;
-        case '/' :
-            priority = 2;
-            break;
-        case '(' :
-            priority = 3;
-            break;
-        case ')' :
-            priority = 3;
-            break;
-        default :
-            break;
+    bool checkOperator(char ch) {
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+            return true;
+        else
+            return false;
     }
-    return priority;
-}
+
+    bool checkLBracket(char ch) {
+        if (ch == '(')
+            return true;
+        else
+            return false;
+    }
+
+    bool checkRBracket(char ch) {
+        if (ch == ')')
+            return true;
+        else
+            return false;
+    }
+
+    bool checkStack(char ch, stack<char> tempStack) {
+        if (tempStack.empty() == true || checkPriority(ch, tempStack.top()) == true)
+            return true;
+        else
+            return false;
+    }
+
+    bool checkPriority(char one, char two) {
+        if (getPriority(one) > getPriority(two))
+            return true;
+        else
+            return false;
+    }
 
 
-string change(string infix, string suffix) {
-    int j = 0;
-    stack<char> operatorStack;
-    for (int i = 0; infix[i] != '#'; i++) {
-        if (checkNum(infix[i])) {
-            suffix[j] = infix[i];
-            j++;
+    int getPriority(char temp) {
+        int priority = 0;
+        switch (temp) {
+            case '+' :
+                priority = 1;
+                break;
+            case '-' :
+                priority = 1;
+                break;
+            case '*' :
+                priority = 2;
+                break;
+            case '/' :
+                priority = 2;
+                break;
+            case '(' :
+                priority = 3;
+                break;
+            case ')' :
+                priority = 3;
+                break;
+            default :
+                break;
         }
-        else {
-            if (!checkRBracket(infix[i])) {
-                while (!checkStack(infix[i], operatorStack) && operatorStack.top() != '(') {
-                    suffix[j] = operatorStack.top();
-                    j++;
-                    operatorStack.pop();
+        return priority;
+    }
 
-                }
-                operatorStack.push(infix[i]);
+    int charToInt(char ch) {
+        int num;
+        num = (int) (ch - '0');
+        return num;
+    }
+
+    int calculate(char opera, int a, int b) {
+        int result = 0;
+
+        switch (opera) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+        }
+
+        return result;
+    }
+
+    bool stateNum(int i, int &lb) {
+        bool error;
+
+        i++;
+
+        if (infix[i] != '#') {
+            if (checkOperator(infix[i])) {
+                error = stateOper(i, lb);
+            }
+            else if (checkRBracket(infix[i])) {
+                error = stateRBra(i, lb);
+            }
+            else
+                error = false;
+        }
+        else
+            error = true;
+
+        return error;
+    }
+
+    bool stateOper(int i, int &lb) {
+        bool error;
+
+        i++;
+
+        if (infix[i] != '#') {
+            if (checkNum(infix[i])) {
+                error = stateNum(i, lb);
+            }
+            else if (checkLBracket(infix[i])) {
+                error = stateLBra(i, lb);
+            }
+            else
+                error = false;
+        }
+        else
+            error = false;
+
+        return error;
+
+    }
+
+    bool stateLBra(int i, int &lb) {
+        bool error;
+
+        i++;
+        lb++;
+
+        if (infix[i] != '#') {
+            if (checkNum(infix[i])) {
+                error = stateNum(i, lb);
+            }
+            else
+                error = false;
+        }
+        else
+            error = false;
+
+        return error;
+    }
+
+    bool stateRBra(int i, int &lb) {
+        bool error;
+
+        i++;
+        lb--;
+
+        if (infix[i] != '#') {
+            if (checkOperator(infix[i])) {
+                error = stateOper(i, lb);
+            }
+            else
+                error = false;
+        }
+        else
+            error = true;
+
+        return error;
+    }
+
+public:
+    Calculate(string s) {
+        infix = s + '#';
+    }
+
+    void inspirit(string s) {
+        infix = s + '#';
+    }
+
+    string change() {
+        string suffix = infix;
+        int j = 0;
+        stack<char> operatorStack;
+        for (int i = 0; infix[i] != '#'; i++) {
+            if (checkNum(infix[i])) {
+                suffix[j] = infix[i];
+                j++;
             }
             else {
-                while (operatorStack.top() != '(') {
-                    suffix[j] = operatorStack.top();
-                    j++;
+                if (!checkRBracket(infix[i])) {
+                    while (!checkStack(infix[i], operatorStack) && operatorStack.top() != '(') {
+                        suffix[j] = operatorStack.top();
+                        j++;
+                        operatorStack.pop();
+
+                    }
+                    operatorStack.push(infix[i]);
+                }
+                else {
+                    while (operatorStack.top() != '(') {
+                        suffix[j] = operatorStack.top();
+                        j++;
+                        operatorStack.pop();
+                    }
                     operatorStack.pop();
                 }
-                operatorStack.pop();
             }
         }
-    }
-    while (!operatorStack.empty()) {
-        suffix[j] = operatorStack.top();
-        j++;
-        operatorStack.pop();
-    }
-
-    suffix[j] = '@';
-
-    int size = suffix.find('@', 0);
-    string subSuffix = suffix.substr(0, size);
-    subSuffix += '#';
-
-    return subSuffix;
-}
-
-int getValue(string suffix) {
-    stack<int> numberStack;
-
-    for (int i = 0; suffix[i] != '#'; i++) {
-        if (checkNum(suffix[i]))
-            numberStack.push(charToInt(suffix[i]));
-        else {
-            int b = numberStack.top();
-            numberStack.pop();
-            int a = numberStack.top();
-            numberStack.pop();
-            numberStack.push(calculate(suffix[i], a, b));
+        while (!operatorStack.empty()) {
+            suffix[j] = operatorStack.top();
+            j++;
+            operatorStack.pop();
         }
+
+        suffix[j] = '@';
+
+        int size = suffix.find('@', 0);
+        string subSuffix = suffix.substr(0, size);
+        subSuffix += '#';
+
+        return subSuffix;
     }
 
-    return numberStack.top();
-}
-
-int charToInt(char ch) {
-    int num;
-    num = (int) (ch - '0');
-    return num;
-}
-
-int calculate(char opera, int a, int b) {
-    int result = 0;
-
-    switch (opera) {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            result = a / b;
-            break;
+    void pri() {
+        cout << infix << endl;
     }
 
-    return result;
-}
-
-bool checkError(string infix) {
-    bool error = true;
-    int leftBracket = 0;
-
-    if (checkNum(infix[0])) {
-        error = stateNum(infix, 0, leftBracket);
-    }
-    else if (checkLBracket(infix[0])) {
-        error = stateLBra(infix, 0, leftBracket);
-    }
-    else
-        error = false;
-
-    if (error == true && leftBracket == 0)
-        return true;
-    else
-        return false;
-
-
-}
-
-bool stateNum(string infix, int i, int &lb) {
-    bool error;
-
-    i++;
-
-    if (infix[i] != '#') {
-        if (checkOperator(infix[i])) {
-            error = stateOper(infix, i, lb);
+    int getValue() {
+        stack<int> numberStack;
+        string suffix = change();
+        for (int i = 0; suffix[i] != '#'; i++) {
+            if (checkNum(suffix[i]))
+                numberStack.push(charToInt(suffix[i]));
+            else {
+                int b = numberStack.top();
+                numberStack.pop();
+                int a = numberStack.top();
+                numberStack.pop();
+                numberStack.push(calculate(suffix[i], a, b));
+            }
         }
-        else if (checkRBracket(infix[i])) {
-            error = stateRBra(infix, i, lb);
+
+        return numberStack.top();
+    }
+
+
+    bool checkError() {
+        bool error = true;
+        int leftBracket = 0;
+
+        if (checkNum(infix[0])) {
+            error = stateNum(0, leftBracket);
+        }
+        else if (checkLBracket(infix[0])) {
+            error = stateLBra(0, leftBracket);
         }
         else
             error = false;
-    }
-    else
-        error = true;
 
-    return error;
-}
-
-bool stateOper(string infix, int i, int &lb) {
-    bool error;
-
-    i++;
-
-    if (infix[i] != '#') {
-        if (checkNum(infix[i])) {
-            error = stateNum(infix, i, lb);
-        }
-        else if (checkLBracket(infix[i])) {
-            error = stateLBra(infix, i, lb);
-        }
+        if (error == true && leftBracket == 0)
+            return true;
         else
-            error = false;
+            return false;
+
+
     }
-    else
-        error = false;
+};
 
-    return error;
+int main() {
+    string input;
 
-}
-
-bool stateLBra(string infix, int i, int &lb) {
-    bool error;
-
-    i++;
-    lb++;
-
-    if (infix[i] != '#') {
-        if (checkNum(infix[i])) {
-            error = stateNum(infix, i, lb);
-        }
-        else
-            error = false;
+    cin >> input;
+    Calculate calculate(input);
+    while (!calculate.checkError()) {
+        cout << "error" << endl;
+        cin >> input;
+        calculate.inspirit(input);
     }
-    else
-        error = false;
+    cout << calculate.change() << endl;
+    cout << calculate.getValue() << endl;
 
-    return error;
-}
-
-bool stateRBra(string infix, int i, int &lb) {
-    bool error;
-
-    i++;
-    lb--;
-
-    if (infix[i] != '#') {
-        if (checkOperator(infix[i])) {
-            error = stateOper(infix, i, lb);
-        }
-        else
-            error = false;
-    }
-    else
-        error = true;
-
-    return error;
 }

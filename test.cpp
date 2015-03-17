10eaa4744d64da9aed07d3b346fef42d312901ff
LYK <1756633219@qq.com>
2015-03-17 15:16:57 +0800

    更新测试文件

--- test.cpp
+++ test.cpp
@@ -14,14 +14,120 @@ int main() {
     calculate.setExpression("(1");
     assert(calculate.isError());
 
+    calculate.setExpression(")");
+    assert(calculate.isError());
+
     calculate.setExpression("1(");
     assert(calculate.isError());
 
     calculate.setExpression("((2+2))");
     assert(!calculate.isError());
 
+    calculate.setExpression("()");
+    assert(calculate.isError());
+
+    calculate.setExpression("(*");
+    assert(calculate.isError());
+
+    calculate.setExpression("(/");
+    assert(calculate.isError());
+
+    calculate.setExpression("(+");
+    assert(calculate.isError());
+
+    calculate.setExpression("(-");
+    assert(calculate.isError());
+
+    calculate.setExpression("+-");
+    assert(calculate.isError());
+
+    calculate.setExpression("*/");
+    assert(calculate.isError());
+
+    calculate.setExpression("-+");
+    assert(calculate.isError());
+
+    calculate.setExpression("++");
+    assert(calculate.isError());
+
+    calculate.setExpression("--");
+    assert(calculate.isError());
+
+    calculate.setExpression("**");
+    assert(calculate.isError());
+
+    calculate.setExpression("//");
+    assert(calculate.isError());
+
+    calculate.setExpression("+*");
+    assert(calculate.isError());
+
+    calculate.setExpression("+/");
+    assert(calculate.isError());
+
+    calculate.setExpression("-*");
+    assert(calculate.isError());
+
+    calculate.setExpression("-/");
+    assert(calculate.isError());
+
+    calculate.setExpression("*+");
+    assert(calculate.isError());
+
+    calculate.setExpression("*-");
+    assert(calculate.isError());
+
+    calculate.setExpression("/*");
+    assert(calculate.isError());
+
+    calculate.setExpression("/+");
+    assert(calculate.isError());
+
+    calculate.setExpression("/-");
+    assert(calculate.isError());
+
+    calculate.setExpression("()");
+    assert(calculate.isError());
+
+    calculate.setExpression("1");
+    assert(!calculate.isError());
+
+    calculate.setExpression("+");
+    assert(calculate.isError());
+
+    calculate.setExpression("-");
+    assert(calculate.isError());
+
+    calculate.setExpression("*");
+    assert(calculate.isError());
+
+    calculate.setExpression("/");
+    assert(calculate.isError());
+
+    calculate.setExpression("(2+3))");
+    assert(calculate.isError());
+
+    calculate.setExpression("((2+3)");
+    assert(calculate.isError());
+
+    calculate.setExpression("22");
+    assert(!calculate.isError());
+
+    calculate.setExpression("(22)");
+    assert(!calculate.isError());
+
+    calculate.setExpression("(+)");
+    assert(calculate.isError());
+
+    calculate.setExpression("2/0");
+    assert(calculate.isError());
 
     //判断正误
+
+    calculate.setExpression("1");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 1);
+
     calculate.setExpression("1+1");
     assert(!calculate.isError());
     assert(calculate.getAnswer()  == 2);
@@ -33,6 +139,38 @@ int main() {
     calculate.setExpression("((2*2)+(2*3))+5");
     assert(!calculate.isError());
     assert(calculate.getAnswer()  == 15);
+
+    calculate.setExpression("1/2");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 0);
+
+    calculate.setExpression("0/2");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 0);
+
+    calculate.setExpression("7*(4+7*(1-2)/2)");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 0);
+
+    calculate.setExpression("22");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 22);
+
+    calculate.setExpression("22-1");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 21);
+
+    calculate.setExpression("1-22");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == -21);
+
+    calculate.setExpression("22+11");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 33);
+
+    calculate.setExpression("11*(22+1/2)-(12+1)");
+    assert(!calculate.isError());
+    assert(calculate.getAnswer()  == 229);
 
     cout<<"Test Passed!"<<endl;
 
